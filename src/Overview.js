6d132cf54d5d09e7eb28fee8257d244014de3d77
Liliana Tuta <lilianatutza@gmail.com>
2024-11-26 21:06:27 +0000

    Overview button link included

--- src/Overview.js
+++ src/Overview.js
@@ -1,6 +1,17 @@
 import React from "react";
 
 const Overview = () => {
-  return <button>Overview</button>;
+  const handleClick = React.useCallback(() => {
+    window.open(
+      "https://www.google.com/search?gs_ssp=eJzj4tDP1Tcwyki2NGD04kzJLE4uzy_KSQEAQI4Gkw&q=discworld&rlz=1C1CHBF_en-GBGB1112GB1114&oq=discw&gs_lcrp=EgZjaHJvbWUqDwgBEC4YQxixAxiABBiKBTIGCAAQRRg7Mg8IARAuGEMYsQMYgAQYigUyGAgCEC4YFBiDARjHARiHAhixAxjRAxiABDIMCAMQIxgnGIAEGIoFMgYIBBBFGDkyBwgFEAAYgAQyBggGEEUYPDIGCAcQRRhB0gEIODM3N2owajeoAgCwAgA&sourceid=chrome&ie=UTF-8",
+      "_blank"
+  );
+  }, []); // Empty dependency array ensures function is stable between renders
+  return (
+    <div>
+      <button onClick={handleClick}>Overview</button>
+    </div>
+  );
 };
+
 export default Overview;
